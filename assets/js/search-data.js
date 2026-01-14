cbc085da87a93b81c016d70f43bdc5e52ff1e3eb
dodeondre <dddo@ucsd.edu>
2026-01-14 22:38:35 +0000

    Deploying to gh-pages from @ dodeondre/dodeondre.github.io@8f0ff7f3ae8cb53ec9056cb9e315b60051d5abe8 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -38,8 +38,8 @@ ninja.data = [{
             window.location.href = "/projects/";
           },
         },{id: "nav-repositories",
-          title: "repositories",
-          description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
+          title: "Repositories",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/repositories/";
